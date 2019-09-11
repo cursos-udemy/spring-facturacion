7f594835090fdd557d9d6e62b3c7f0ee4e9827cc
gwfernandez <gwfernandez@gmail.com>
2019-09-10 22:46:10 -0300

    commit inicial

---
+++ src/main/java/com/jendrix/udemy/facuracion/app/UdemyFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.jendrix.udemy.facuracion.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyFacturacionApplication.class, args);
+	}
+
+}
